8f7bb5f286a683bdd3f0bb01c9dc57abd24b672f
sentencija <supmaks8654@gmail.com>
2025-01-27 14:13:37 +0100

    restructure into modules

---
+++ minesweeper/basic.lua
@@ -0,0 +1,164 @@
+local mineCountParamName = "Mine count"
+basicModule = {
+	params = {
+		mine = {
+			[mineCountParamName] = function (w, h, params)
+				return {min = 1, max = w * h - (w<3 and w or 3) * (h<3 and h or 3)}
+			end
+		},
+		empty = {},
+		draw = {
+			size = {1,1},
+			backgroundColor = {1,1,1},
+			borderColor = {0.5,0.5,0.5}
+		}
+	}
+}
+function getModuleObject() return basicModule end		
+function basicModule.init(w, h, firstClickPosition, mineParams, emptyParams)
+	local firstClickX = firstClickPosition[1]
+	local firstClickY = firstClickPosition[2]
+	local cells = basicModule.getCellSet(w, h, firstClickX, firstClickY) 
+	local field = basicModule.createField(w, h)
+	local dangerous = {}
+	for i=1,mineParams[mineCountParamName] do
+		local chosenCell = table.remove(cells, math.random(#cells))
+		field[chosenCell[1]][chosenCell[2]].mine = true
+		table.insert(dangerous, chosenCell)
+	end
+	local startAdjacent = basicModule.getAdjacentCells(firstClickX, firstClickY, w, h)
+	table.insert(startAdjacent, firstClickPosition)
+	for i=1,#startAdjacent do table.insert(cells, startAdjacent[i]) end
+	for i=1,#cells do
+		local thisCell = cells[i]
+		local counter = 0
+		local thisCellX = thisCell[1]
+		local thisCellY = thisCell[2]
+		local adjacent = basicModule.getAdjacentCells(thisCellX, thisCellY, w, h)
+		for j=1,#adjacent do
+			local thisAdjacent = adjacent[j]
+			if field[thisAdjacent[1]][thisAdjacent[2]].mine then counter = counter + 1 end
+		end
+		field[thisCellX][thisCellY].number = counter
+	end
+	return field, dangerous 
+end
+function basicModule.emptyCheck(x, y, field)
+	local c = field[x][y]
+	if c.number == nil then return {} end
+	local adjacent = basicModule.getAdjacentCells(x, y, #field, #field[1])
+	local result = {}
+	local counter = c.number 
+	print("counter " .. counter)
+	if counter == 0 then 
+		for i=1,#adjacent do
+			table.insert(result, adjacent[i])
+		end
+		return result
+	end
+	for i=1,#adjacent do
+		local cell = adjacent[i]
+		local cellX = cell[1]
+		local cellY = cell[2]
+		if cell.mine then break end
+		if minesweeper.global.isFlagged(cellX, cellY) then 
+			counter = counter - 1
+		elseif not minesweeper.global.isOpened(cellX, cellY) then
+			table.insert(result, cell)
+		end
+	end
+	print("counter after " .. counter)
+	if counter ~= 0 then return {} end
+	return result
+end
+function basicModule.createField(w, h)
+	local field = {}
+	for i=1,w do
+		local line = {}
+		for j=1,h do table.insert(line, {}) end
+		table.insert(field, line)
+	end
+	return field
+end
+function basicModule.getCellSet(w, h, firstClickX, firstClickY)
+	local firstClickAdjacentCells = basicModule.getAdjacentCells(firstClickX, firstClickY, w, h)
+	table.insert(firstClickAdjacentCells, {firstClickX, firstClickY})
+	local possibleCells = {}
+	for i=1,w do
+		for j=1,h do
+			local skip = false
+			for k=1,#firstClickAdjacentCells do
+				local cell = firstClickAdjacentCells[k]
+				if cell[1] == i and cell[2] == j then
+					skip = true
+					break
+				end
+			end
+			if not skip then 
+				table.insert(possibleCells, {i,j})
+			end
+		end
+	end
+	return possibleCells
+end
+function basicModule.getAdjacentCells(x,y,w,h)
+	local possibleXDirections = {}
+	if x ~= 1 then table.insert(possibleXDirections, -1) end
+	if x ~= w then table.insert(possibleXDirections, 1) end
+	local possibleYDirections = {}
+	if y ~= 1 then table.insert(possibleYDirections, -1) end
+	if y ~= h then table.insert(possibleYDirections, 1) end
+	local result = {}
+	for i=1,#possibleYDirections do table.insert(result, {0,possibleYDirections[i]}) end
+	for i=1,#possibleXDirections do
+		local xDirection = possibleXDirections[i]
+		table.insert(result, {xDirection, 0})
+		for j=1,#possibleYDirections do table.insert(result, {xDirection, possibleYDirections[j]}) end
+	end
+	for i=1,#result do
+		local direction = result[i]
+		direction[1] = direction[1] + x
+		direction[2] = direction[2] + y
+	end
+	return result
+end
+
+function basicModule.drawSmall(cell, x, y, w)
+	local cellNumber = cell.number
+	if cell.mine then drawMine(x, y, w)
+	elseif cellNumber ~= 0 then drawNumber(x, y, w, cellNumber)
+	end
+end
+local sqrt2 = math.sqrt(2)
+function drawMine(x, y, w)
+	love.graphics.setColor(0,0,0)
+	local halfW = w / 2 
+	local centerX = x + halfW
+	local centerY = y + halfW
+	local sqrt2W = halfW / sqrt2
+	love.graphics.circle("fill", centerX, centerY, halfW * 9 / 10)
+	local leftX = centerX - sqrt2W
+	local topY = centerY - sqrt2W
+	local rightX = centerX + sqrt2W
+	local botY = centerY + sqrt2W
+	love.graphics.line(leftX, topY, rightX, botY)
+	love.graphics.line(leftX, botY, rightX, topY)
+	love.graphics.line(x, centerY, x+w, centerY)
+	love.graphics.line(centerX, y, centerX, y+w)
+end
+local fonts = {}
+function drawNumber(x, y, w, number)
+	setFont(w)
+	love.graphics.setColor(0,0,0)
+	love.graphics.print(number, x + w/3, y)
+end
+function setFont(size)
+	if fonts[size] == nil then fonts[size] = love.graphics.newFont(size) end
+	love.graphics.setFont(fonts[size])
+end
+function basicModule.drawBig(cell, x, y, w, h)
+	love.graphics.setColor(0,0,0)
+	setFont(math.min(w,h))
+	love.graphics.print(cell.number, x, y)
+end
+
--- minesweeper/main.lua
+++ minesweeper/main.lua
@@ -1,79 +1,55 @@
 fieldWidth = 10
 fieldHeight = 10
 margin = 100
+marginX = 300
 textSize = 10
 mineCount = 10 
-function createCell()
-	return {
-		mine = false,
-		number = 0,
-		opened = false,
-		flagged = false,
-	}
+bigCellWidth = 3
+bigCellHeight = 3
+moduleNames = {
+	"basic"
+}
+visibleModule = moduleNames[1]
+modules = {}
+for i=1,#moduleNames do
+	local moduleName = moduleNames[i]
+	require(moduleName)
+	modules[moduleName] = {module = getModuleObject()}
 end
-function init()
+minesweeper = {
+	global = {
+		isFlagged = function (x, y)
+			return field[x][y].flagged
+		end,
+		isOpened = function(x,y)
+			return field[x][y].opened
+		end
+	}
+}
+local function init()
 	field = {}
 	for i=1,fieldWidth do
-		local array = {}
-		for j=1,fieldHeight do
-			array[j] = createCell()	
-		end
-		field[i] = array
+		local line = {}
+		for j=1,fieldHeight do table.insert(line, {flagged = false, opened = false, mine = false}) end
+		table.insert(field, line)
 	end
 	gameResult = 0
 	flagCount = 0
 	firstPress = true
 	textFont = love.graphics.newFont(textSize)
+	cellSelected = nil
 end
-function spawnMines(count)
-	local possibleSquares = {}
-	local startDirections = getPossibleDirections(firstPressX, firstPressY)
-	for i=1,fieldWidth do
-		for j=1,fieldHeight do
-			if i ~= firstPressX or j ~= firstPressY then
-				local found = false
-				for k=1,#startDirections do
-					local thisDirection = startDirections[k]
-					if i == firstPressX + thisDirection[1] and j == firstPressY + thisDirection[2] then 
-						found = true
-						break 
-					end
-				end	
-				if not found then
-					table.insert(possibleSquares, {i,j})
-				end
-			end
+function runModulesInit()
+	local firstPress = {firstPressX, firstPressY}
+	for i,j in pairs(modules) do
+		j.field, dangerous = j.module.init(fieldWidth, fieldHeight, firstPress, {["Mine count"] = mineCount}, {})
+		dumpField(i)
+		for k=1,#dangerous do
+			local pos = dangerous[k]
+			field[pos[1]][pos[2]].mine = true
 		end
 	end
-	minePositions = {}
-	for i=1,mineCount do
-		local randomIndex = math.random(table.getn(possibleSquares))
-		local element = table.remove(possibleSquares, randomIndex)
-		table.insert(minePositions, element)
-		field[element[1]][element[2]].mine = true
-	end
-end
-function setNumbers()
-	for i=1,fieldWidth do
-		for j=1,fieldHeight do
-			if not field[i][j].mine then
-				local directions = getPossibleDirections(i,j)	
-				local counter = 0
-				for k=1,#directions do
-					local direction = directions[k]
-					if field[i+direction[1]][j+direction[2]].mine then counter = counter + 1 end
-				end
-				field[i][j].number = counter
-			end
-		end
-	end
-end
-function checkAllFlagged()
-	for i=1,#minePositions do
-		local thisPosition = minePositions[i]
-		if not field[thisPosition[1]][thisPosition[2]].flagged then return end
-	end
-	win()
+	constructBigCell()
 end
 function checkAllOpened()
 	for i=1,fieldWidth do
@@ -85,61 +61,44 @@ function checkAllOpened()
 	win()
 end
 function win() gameResult = 1 end
-function getPossibleDirections(x,y)
-	local availableXDirections = {}
-	if x~=1 then availableXDirections[1] = -1 end
-	if x~=fieldWidth then table.insert(availableXDirections, 1) end
-	local availableYDirections = {}
-	if y~=1 then availableYDirections[1] = -1 end
-	if y~=fieldHeight then table.insert(availableYDirections, 1) end
-	local directions = {}
-	for k=1,#availableYDirections do
-		table.insert(directions, {0, availableYDirections[k]})
-	end
-	for k=1,#availableXDirections do
-		local xDirection = availableXDirections[k]
-		table.insert(directions, {xDirection, 0})
-		for o=1,#availableYDirections do
-			table.insert(directions, {xDirection, availableYDirections[o]})
-		end
-	end
-	return directions
-end
-function open(x,y, singleOpen)
-	local thisCell = field[x][y]
-	thisCell.opened = true
-	field[x][y] = thisCell
-	if thisCell.mine then 
-		gameResult = -1
+function lose() gameResult = -1 end
+function open(x, y)
+	local cell = field[x][y]
+	cell.opened = true
+	if cell.mine then 
+		lose()
 		return
 	end
-	local count = thisCell.number
-	local directions = getPossibleDirections(x,y)
-	local unflagged = {}
-	for i=1,#directions do
-		local thisDirection = directions[i]
-		local newX = x+thisDirection[1]
-		local newY = y+thisDirection[2]
-		local thisDirectionCell = field[newX][newY] 
-		if thisDirectionCell.flagged then count = count - 1
-		elseif not thisDirectionCell.opened then table.insert(unflagged, {newX, newY})  
-		end
-	end
-	if count == 0 then
-		for i=1,#unflagged do
-			local nextCell = unflagged[i]	
-			local cell = field[nextCell[1]][nextCell[2]]
-			if not singleOpen or cell.number == 0 then 
-				open(nextCell[1],nextCell[2])
-			else 
-				cell.opened = true
-				field[nextCell[1]][nextCell[2]] = cell
+	for i,j in pairs(modules) do
+		local nextCells = j.module.emptyCheck(x,y, j.field)
+		print("next size " .. #nextCells)
+		while #nextCells ~= 0 do
+			local replacement = {}
+			for k=1,#nextCells do
+				local nextCell = nextCells[k]
+				local nextCellX = nextCell[1]
+				local nextCellY = nextCell[2]
+				local checkedCell = field[nextCellX][nextCellY]
+				if not checkedCell.opened and not checkedCell.flagged then 
+					field[nextCellX][nextCellY].opened = true
+					if field[nextCellX][nextCellY].mine then 
+						lose()
+						return
+					end
+					local nextNextCells = j.module.emptyCheck(nextCellX, nextCellY, j.field)
+					for o=1,#nextNextCells do
+						table.insert(replacement, nextNextCells[o]) 
+					end
+					
+				end
 			end
+			nextCells = replacement
 		end
+		break
 	end
 end
 function calculateScreenValues()
-	local cellWidth = (width - margin * 2) / fieldWidth 
+	local cellWidth = (width - margin * 3 - marginX) / fieldWidth 
 	local cellHeight = (height - margin * 2) / fieldHeight
 	cellSize = math.min(cellWidth, cellHeight)
 	cellBorder = cellSize / 10
@@ -148,31 +107,134 @@ function calculateScreenValues()
 	halfCellSize = cellSize / 2
 	fieldViewWidth = cellSize*fieldWidth
 	fieldViewHeight = cellSize*fieldHeight
-	widthOffset = (width - fieldViewWidth) / 2
+	widthOffset = ((width - margin - marginX - fieldViewWidth) / 2) + margin + marginX
 	heightOffset = (height - fieldViewHeight) / 2
+
+	local largerBigCellSize = math.max(bigCellWidth, bigCellHeight)
+	bigCellMargin = marginX / (10 * largerBigCellSize) 
+	bigCellSize = (marginX - bigCellMargin) / largerBigCellSize 
 end
 function love.load()
-	width = 960
-	height = 540
-	love.window.setMode(960,540,{})
+	width = 1600 
+	height = 900 
+	love.window.setMode(width,height,{})
 	calculateScreenValues()
 	love.keyboard.setKeyRepeat(true)
 	init()
 end
-function line(x1,y1,x2,y2)
-	love.graphics.line(widthOffset+x1, heightOffset+y1, widthOffset+x2, heightOffset+y2)
-end
-function rect(mode, x, y, w, h)
-	love.graphics.rectangle(mode, widthOffset+x, heightOffset+y, w, h)
-end
-function circle(mode, x, y, r)
-	love.graphics.circle(mode, widthOffset+x, heightOffset+y, r)
-end
-function text(text, x, y)
-	love.graphics.print(text, widthOffset+x, heightOffset+y)
-end
 function color(r,g,b)
 	love.graphics.setColor(r,g,b,1)
+end
+function colorV(v)
+	color(v[1],v[2],v[3])
+end
+function drawCell(cellX,cellY, thisCell, fieldX, fieldY)
+	local rect = function (x,y,w,h) love.graphics.rectangle("fill", widthOffset+x, heightOffset+y, w, h) end
+	local line = function (x1,y1,x2,y2) love.graphics.line(widthOffset+x1, heightOffset+y1, widthOffset+x2, heightOffset+y2) end
+	color(0.5,0.5,0.5)
+	rect(cellX, cellY, cellSize, cellSize) --cell border 
+	if thisCell.opened then color(1,1,1) else color(0,0,0) end 
+	local cellInnerX = cellX + cellBorder
+	local cellInnerY = cellY + cellBorder
+	rect(cellInnerX, cellInnerY, cellSize - doubleCellBorder, cellSize - doubleCellBorder) --cell inner square 
+	if thisCell.opened then 
+		local visible = modules[visibleModule]
+		local moduleCell = visible.field[fieldX][fieldY]
+		visible.module.drawSmall(moduleCell, widthOffset + cellInnerX, heightOffset + cellInnerY, cellSize - doubleCellBorder)
+	else
+		if thisCell.flagged then
+			local cellInnerSize = cellSize - doubleCellBorder 
+			local cellInnerUpperX = cellInnerX + cellBorder
+			local cellInnerUpperY = cellInnerY + cellBorder
+			local cellInnerLowerX = cellX + cellInnerSize 
+			local cellInnerLowerY = cellY + cellInnerSize 
+			color(1,0,0)
+			line(cellInnerUpperX, cellInnerUpperY, cellInnerLowerX, cellInnerLowerY)
+			line(cellInnerUpperX, cellInnerLowerY, cellInnerLowerX, cellInnerUpperY)
+		end
+	end
+end
+function drawBigCell(cellX, cellY)
+	local rect = function(offset) love.graphics.rectangle("fill",offset+margin, offset+margin, marginX-offset*2, marginX-offset*2, cellBorder) end
+	color(0.5,0.5,0.5)
+	rect(0)
+	color(1,1,1)
+	rect(cellBorder)
+	for i=1,#bigCellComposition do
+		local bigCell = bigCellComposition[i]
+		local position = bigCell.position
+		local bigCellS = bigCellSize + bigCellMargin
+		local x = margin + (position[1] - 1) * bigCellS + bigCellMargin
+		local y = margin + (position[2] - 1) * bigCellS + bigCellMargin
+		local size = bigCell.size
+		local w = bigCellS * (size[1] - 1) + bigCellSize
+		local h = bigCellS * (size[2] - 1) + bigCellSize
+		colorV(bigCell.borderColor)
+		love.graphics.rectangle("fill", x, y, w, h, bigCellMargin)
+		local innerX = x + bigCellMargin
+		local innerY = y + bigCellMargin
+		local doubleMargin = bigCellMargin * 2
+		local innerW = w - doubleMargin
+		local innerH = h - doubleMargin
+		colorV(bigCell.backgroundColor)
+		love.graphics.rectangle("fill", innerX, innerY, innerW, innerH, bigCellMargin)
+		local module = modules[bigCell.module]
+		module.module.drawBig(module.field[cellX][cellY], innerX, innerY, innerW, innerH)
+	end
+end
+function constructBigCell()
+	local fittingArray = {}
+	for i=1,bigCellWidth do
+		local line = {}
+		for j=1,bigCellHeight do
+			table.insert(line, false)
+		end
+		table.insert(fittingArray, line)
+	end
+	local moduleCells = {}
+	for i,j in pairs(modules) do
+		table.insert(moduleCells, {size = j.module.params.draw.size, module = i})
+	end
+	table.sort(moduleCells, function(a,b) return a.size[1] * a.size[2] > b.size[1] * b.size[2] end)
+	local result = {}
+	for i=1,#moduleCells do
+		local cell = moduleCells[i]
+		local found = false
+		for j=1,#fittingArray do
+			if found then break end
+			for k=1,#fittingArray[j] do
+				local fits = checkFitting(fittingArray, cell, j, k)
+				if fits then
+					for o=1,cell.size[1] do
+						for p=1,cell.size[2] do
+							fittingArray[j+o-1][k+p-1] = true
+						end
+					end
+					local module = cell.module
+					local moduleParams = modules[module].module.params.draw
+					table.insert(result, {position = {j,k}, size = cell.size, module = module, backgroundColor = moduleParams.backgroundColor, borderColor = moduleParams.borderColor}) 
+					found = true
+					break
+				end
+			end
+		end
+	end
+	bigCellComposition = result
+end
+function checkFitting(array, element, x, y)	
+	local elementW = element.size[1]
+	local elementH = element.size[2]
+	local w = #array
+	local h = #array[1]
+	if x+elementW > w or y+elementH > h then return false end
+	for i=1,elementW do
+		local newX = x + i - 1
+		for j=1,elementH do
+			local newY = y + j - 1
+			if array[newX][newY] then return false end
+		end
+	end
+	return true
 end
 gameContinueText = "Game is still in progress"
 gameWonText = "You have won!"
@@ -188,33 +250,11 @@ function love.draw()
 		for j=1,fieldHeight do
 			local cellX = (i-1) * cellSize
 			local cellY = (j-1) * cellSize
-			love.graphics.setColor(0.5,0.5,0.5)
-			rect("fill", cellX, cellY, cellSize, cellSize) --cell border 
-			local thisCell = field[i][j]
-			if thisCell.opened then love.graphics.setColor(1,1,1) else love.graphics.setColor(0,0,0) end 
-			local cellInnerX = cellX + cellBorder
-			local cellInnerY = cellY + cellBorder
-			rect("fill", cellInnerX, cellInnerY, cellSize - doubleCellBorder, cellSize - doubleCellBorder) --cell inner square 
-			if thisCell.opened then 
-				if thisCell.mine then
-					color(0,0,0)
-					circle("fill", cellX+halfCellSize, cellY+halfCellSize, cellSize/2-cellBorder)
-				else
-					color(0,0,0)	
-					local thisCellNumber = thisCell.number
-					if thisCellNumber ~= 0 then text(thisCellNumber, cellX + halfCellSize / 2, cellY) end
-				end
-			else
-				if thisCell.flagged then
-					local cellInnerSize = cellSize - cellBorder
-					local cellInnerLowerX = cellX + cellInnerSize 
-					local cellInnerLowerY = cellY + cellInnerSize 
-					color(1,0,0)
-					line(cellInnerX, cellInnerY, cellInnerLowerX, cellInnerLowerY)
-					line(cellInnerX, cellInnerLowerY, cellInnerLowerX, cellInnerY)
-				end
-			end
+			drawCell(cellX, cellY, field[i][j], i, j)
 		end
+	end
+	if cellSelected ~= nil then
+		drawBigCell(cellSelected[1],cellSelected[2])	
 	end
 end
 function love.mousepressed(x,y,button,istouch)
@@ -229,15 +269,17 @@ function love.mousepressed(x,y,button,istouch)
 						firstPress = false
 						firstPressX = fieldX
 						firstPressY = fieldY
-						spawnMines(mineCount)
-						setNumbers()
+						runModulesInit()
 					end
-					open(fieldX,fieldY,cell.opened)
+					open(fieldX,fieldY)
 					checkAllOpened()
-				elseif not cell.opened then
-					cell.flagged = not cell.flagged
-					flagCount = flagCount + (cell.flagged and 1 or -1)
-					checkAllFlagged()
+				else
+					if not cell.opened then
+						cell.flagged = not cell.flagged
+						flagCount = flagCount + (cell.flagged and 1 or -1)
+					else
+						cellSelected = {fieldX, fieldY}
+					end
 				end
 			end
 		else
@@ -245,11 +287,22 @@ function love.mousepressed(x,y,button,istouch)
 		end
 	end	
 end
+function dumpField(moduleName)
+	local moduleField = modules[moduleName].field
+	for i=1,#moduleField do
+		for j=1,#moduleField[i] do
+			for k,o in pairs(moduleField[i][j]) do
+				print("element" .. i .. "," .. j .. ": " .. tostring(k) .. "=" .. tostring(o))
+			end
+		end
+	end
+end
 function maxMineCount() return fieldWidth * fieldHeight - ((fieldWidth < 3 and fieldWidth or 3) * (fieldHeight < 3 and fieldHeight or 3)) end
 function limitMineCount()
 	local max = maxMineCount()
 	if mineCount > max then mineCount = max end
 end
+function possibleSize(decreaseWidth) return fieldWidth + (decreaseWidth and -1 or 0) > 3 or fieldHeight + (decreaseWidth and 0 or -1) > 3 end
 function love.keypressed(key, scancode, isrepeat)
 	if key == "c" then
 		for i=1,fieldWidth do
@@ -259,6 +312,8 @@ function love.keypressed(key, scancode, isrepeat)
 				field[i][j] = thisCell
 			end
 		end
+	elseif key == "r" then
+		init()
 	elseif firstPress then
 		if key == "m" and mineCount < maxMineCount() then mineCount = mineCount + 1 
 		elseif key == "l" and mineCount > 1 then mineCount = mineCount - 1 
@@ -266,8 +321,8 @@ function love.keypressed(key, scancode, isrepeat)
 			if key == "s" then fieldHeight = fieldHeight + 1
 			elseif key == "d" then fieldWidth = fieldWidth + 1
 			else
-				if key == "w" and fieldHeight > 1 then fieldHeight = fieldHeight - 1 
-				elseif key == "a" and fieldWidth > 1 then fieldWidth = fieldWidth - 1 end
+				if key == "w" and possibleSize(false) and fieldHeight > 1 then fieldHeight = fieldHeight - 1 
+				elseif key == "a" and possibleSize(true) and fieldWidth > 1 then fieldWidth = fieldWidth - 1 end
 				limitMineCount()
 			end
 			init()
@@ -275,4 +330,15 @@ function love.keypressed(key, scancode, isrepeat)
 		end
 	end
 end
-
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
